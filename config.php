39f9e9b2f09559899bf09cc1fe55c8b316e46d43
root <root@casa.(none)>
2012-10-15 18:07:50 -0300

    subiendo todo

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ingenyo.dyndns.tv/weke/');
+define('HTTP_IMAGE', 'http://ingenyo.dyndns.tv/weke/image/');
+define('HTTP_ADMIN', 'http://ingenyo.dyndns.tv/weke/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ingenyo.dyndns.tv/weke/');
+define('HTTPS_IMAGE', 'http://ingenyo.dyndns.tv/weke/image/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/weke/catalog/');
+define('DIR_SYSTEM', '/var/www/weke/system/');
+define('DIR_DATABASE', '/var/www/weke/system/database/');
+define('DIR_LANGUAGE', '/var/www/weke/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/weke/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/weke/system/config/');
+define('DIR_IMAGE', '/var/www/weke/image/');
+define('DIR_CACHE', '/var/www/weke/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/weke/download/');
+define('DIR_LOGS', '/var/www/weke/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'im');
+define('DB_DATABASE', 'opencart_new');
+define('DB_PREFIX', '');
+?>
